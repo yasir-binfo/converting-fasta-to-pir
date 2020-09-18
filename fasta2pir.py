943cab1dbfde0a2768ec31f685cabe227b9f856a
yasir-binfo <yasir.binfo@gmail.com>
2020-09-18 08:33:03 +0000

    modified fasta to pir

--- fasta2pir.py
+++ fasta2pir.py
@@ -4,32 +4,36 @@ import glob
 files = glob.glob('*.fasta')
 
 for file in files:
-	with open(file,'r') as f:
+    with open(file,'r') as f:
     
-    		l = f.readlines()
+        l = f.readlines()
     
-    		n = ''
+        n = ''
     
-    		out =  file.split('.')[0] + '_pir.pir'
+        out =  file.split('.')[0] + '_pir.pir'
     
-    		for x in range(len(l)):
+        for x in range(len(l)):
         
-        		if '>' in l[x]:
-				if n != '':
-					n_list = list(n)
-					n_list[-1] = '*'
-					n_list.append('\n')
-					n = ''.join(n_list)
+            if '>' in l[x]:
+                    if n != '':
+                        n_list = list(n)
+                        n_list[-1] = '*'
+                        n_list.append('\n')
+                        n = ''.join(n_list)
                 
-            			s =  l[x].split()[0][1:]
+                    s =  l[x].split()[0][1:]
                 
-            			p = '>P1;' + s + '\n' + 'sequence:' + s + '::::::::' + '\n'
+                    p = '>P1;' + s + '\n' + 'sequence:' + s + '::::::::' + '\n'
                 
-            			l[x] = p
+                    l[x] = p
                 
-        		n += l[x]
-            
-	with open(out,'w') as w:
-    		w.write(n)
+            n += l[x]
+
+n_list = list(n)
+n_list[-1] = '*'
+n_list.append('\n')
+n = ''.join(n_list)
+with open(out,'w') as w:
+    w.write(n)
         
         
